2cd0e88b4ec9829536f81e5d9fd7207e2ac3fb7f
Matt <matt.thompson1991@gmail.com>
2019-08-13 14:37:54 +1000

    Added BOM as source in core

--- rpi_metar/core.py
+++ rpi_metar/core.py
@@ -50,7 +50,7 @@ def fetch_metars(queue, cfg):
 
     airport_codes = list(AIRPORTS.keys())
     data_sources = []
-    for source in [sources.NOAA, sources.NOAABackup, sources.SkyVector]:
+    for source in [sources.NOAA, sources.NOAABackup, sources.SkyVector, sources.BOM]:
         try:
             data_sources.append(source(airport_codes))
         except:
